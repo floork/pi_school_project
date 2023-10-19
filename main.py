03d73bc15afe527d21c31ad46283653c2daa1f48
Florian Mros <florian_alexander.mros@tu-dresden.de>
2023-10-19 11:05:59 +0200

    add(csv reader )

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-import RPi.GPIO as GPIO
-
-import dht11
-
 import time
 
-import board
-
-import busio
-
 import adafruit_character_lcd.character_lcd_i2c as character_lcd
-
-
+import board
+import busio
+import dht11
+import RPi.GPIO as GPIO
 
 # Definiere LCD Zeilen und Spaltenanzahl.
 
@@ -19,11 +13,9 @@ lcd_columns = 16
 lcd_rows = 2
 
 
-
 # Initialisierung I2C Bus
 
 i2c = busio.I2C(board.SCL, board.SDA)
-
 
 
 # Festlegen des LCDs in die Variable LCD
@@ -31,24 +23,33 @@ i2c = busio.I2C(board.SCL, board.SDA)
 lcd = character_lcd.Character_LCD_I2C(i2c, lcd_columns, lcd_rows, 0x21)
 
 
+import csv
 
+
+def csvWriter(file_path: str, obj: dict):
+    """
+
+    Schreibt die übergebenen Werte in eine CSV-Datei.
+
+    """
+
+    # Öffnen der Datei im Schreibmodus
+
+    with open(file_path, "a") as file:
+        # Schreiben der Werte in die Datei
+        write = csv.DictWriter(file, fieldnames=obj.keys())
+        write.writerow(obj)
 
 
 def lcd_print(output: str, output2: str):
-
     try:
-
         # Hintergrundbeleuchtung einschalten
 
         lcd.backlight = True
 
-
-
         # Zwei Worte mit Zeilenumbruch werden ausgegeben
 
         lcd.message = output + "\n" + output2
-
-
 
         time.sleep(10)
 
@@ -56,10 +57,7 @@ def lcd_print(output: str, output2: str):
 
         lcd.clear()
 
-
-
     except KeyboardInterrupt:
-
         # LCD ausschalten.
 
         lcd.clear()
@@ -67,11 +65,7 @@ def lcd_print(output: str, output2: str):
         lcd.backlight = False
 
 
-
-
-
 def main():
-
     # initialize GPIO
 
     GPIO.setwarnings(False)
@@ -80,34 +74,23 @@ def main():
 
     GPIO.cleanup()
 
-
-
     # read data using pin 14
 
     instance = dht11.DHT11(pin=4)
 
     result = instance.read()
 
-
-
     while not result.is_valid():  # read until valid values
-
         result = instance.read()
 
-
-
     for i in range(9):
-
         lcd_print(
-
-            "Temp: %-3.1f C" % result.temperature, "Humidity: %-3.1f %%" % result.humidity
-
+            "Temp: %-3.1f C" % result.temperature,
+            "Humidity: %-3.1f %%" % result.humidity,
         )
 
 
-
-#if __name__ == __main__:
+# if __name__ == __main__:
 #
 #    main()
 main()
-
