82a72a2107c168bbbf4f45aa9e05bf7b8973e668
Florian Mros <florian_alexander.mros@tu-dresden.de>
2024-01-22 08:14:49 +0100

    wip

--- csv.py
+++ csv.py
@@ -4,16 +4,12 @@ import csv
 class CSV:
     def __init__(self, filename):
         self.filename = filename
-        self.file = open(filename, "w")
 
-    def __del__(self):
-        self.file.close()
+    def write(self, data: dict):
+        csvfile = open(self.filename, "a")
+        c = csv.DictWriter(csvfile, fieldnames=data.keys())
+        c.writeheader()
 
-    def write(self,  data: dict):
-        file = self.file
-        with open(file, 'w', newline='') as file:
-            writer = csv.DictWriter(file, fieldnames=data.keys()
+        c.writerow(data)
 
-            writer.writeheader()
-            for header in data.keys():
-                writer.writerow(data[header])
+        csvfile.close()
