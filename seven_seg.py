104cc7628a3092c32478892fe118847e62ac7aa9
Florian Mros <florian_alexander.mros@tu-dresden.de>
2024-01-22 07:40:02 +0100

    wip

--- seven_seg.py
+++ seven_seg.py
@@ -15,13 +15,14 @@ class SegementLed:
     def __del__(self):
         self.segment.fill(0)
 
-    def print(self, data: str, data_type: str) -> None:
+    def print(self, data: float, data_type: str) -> None:
         """
         Daten auf LED Panel ausgeben
         """
         self.segment.fill(0)
 
-        data_list = list(data)
+        data_str = str(data)
+        data_list = list(data_str)
 
         # for convenience we print the data to the terminal
         print(data)
