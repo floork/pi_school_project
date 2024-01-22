6fb35bd1cfcb131309caa41902fcea5e748a8896
Florian Mros <florian_alexander.mros@tu-dresden.de>
2024-01-22 08:05:07 +0100

    wip

--- csv.py
+++ csv.py
@@ -5,10 +5,9 @@ class CSV:
     def __init__(self, filename):
         self.filename = filename
         self.file = open(filename, "w")
-        self.csv = csv
 
     def __del__(self):
         self.file.close()
 
     def write(self, data: dict):
-        self.csv.DictWriter(self.file, fieldnames=data.keys())
+        csv.DictWriter(self.file, fieldnames=data.keys())
