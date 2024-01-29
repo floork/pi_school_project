727af093bcc69e220a6ff57b3d46d3ebfc55475a
Florian Mros <florian_alexander.mros@tu-dresden.de>
2024-01-29 07:54:03 +0100

    wip

--- main.py
+++ main.py
@@ -1,31 +1,18 @@
 """
 This is the main file to read the data from the sensor and display it on the 7 segment led panel.
 """
-import csv
-import datetime
 import time
-from datetime import timezone
 
-from database import Database
 from lcd import LcdScreen
 from matrix import Matrix
 from seven_seg import SegementLed
 from temp_humid_sensor import DHT11
 
 
-def csv_writer(data, path):
-    csv_file = open(path, "a", newline="")
-    writer = csv.DictWriter(csv_file, fieldnames=data.keys())
-    writer.writerow(data)
-    csv_file.close()
-
-
 def main():
     """
     Main Funktion des Programms
     """
-    db = Database("database.db")
-    db.create_table("My Data", ["time", "temp", "humidity", "light"])
 
     # Read data from sensor
     dht11 = DHT11()
@@ -36,36 +23,6 @@ def main():
             humudity = data["humidity"]
             temp = data["temp"]
             light = data["light"]
-
-<<<<<<< HEAD
-            # Print data on the 7 segment led panel
-            segment_led = SegementLed()
-            print(type(data["humidity"]))
-
-            segment_led.print(data["humidity"], "humidity")
-            time.sleep(10)
-            segment_led.print(data["temp"], "temp")
-            time.sleep(10)
-=======
-            dt = datetime.datetime.now(timezone.utc)
-            utc_time = dt.replace(tzinfo=timezone.utc)
-            utc_timestamp = utc_time.timestamp()
-
-            full_dict = {
-                "time": utc_timestamp,
-                "temp": temp,
-                "humidity": humudity,
-                "light": light,
-            }
-
-            csv_writer(full_dict, "data.csv")
-
-            # Save data in database
-            db.insert_data(
-                "My Data",
-                full_dict,
-            )
->>>>>>> database
 
             # Print data on the matrix
             matrix = Matrix()
