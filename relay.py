19174741424f9cf038d6f100735f150adecaf440
Florian Mros <florian_alexander.mros@tu-dresden.de>
2024-03-25 08:31:44 +0100

    wip

--- relay.py
+++ relay.py
@@ -4,7 +4,7 @@ import RPi.GPIO as GPIO
 class Relay:
     def __init__(self):
         self.relay_pin = 40
-        GPIO.setmode(GPIO.BMC)
+        GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.relay_pin, GPIO.OUT)
 
     def __del__(self):
