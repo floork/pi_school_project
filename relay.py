651e6dbdc33dafc7ab0c054ce929e25a45748b20
Florian Mros <florian_alexander.mros@tu-dresden.de>
2024-04-15 07:52:53 +0200

    wip

--- relay.py
+++ relay.py
@@ -5,13 +5,12 @@ class Relay:
     def __init__(self):
         self.relay_pin = 21
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.relay_pin, GPIO.OUT)
-
-    def __del__(self):
         GPIO.cleanup()
 
     def open(self):
+        GPIO.setup(self.relay_pin, GPIO.OUT)
         GPIO.output(self.relay_pin, GPIO.LOW)
 
     def close(self):
+        GPIO.setup(self.relay_pin, GPIO.OUT)
         GPIO.output(self.relay_pin, GPIO.HIGH)
